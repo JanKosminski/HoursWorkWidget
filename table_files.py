f2db209f6edc2abb7ecb64db6ae8fa9e64633a35
Ian <jan.kosminski@gmail.com>
2024-03-02 11:13:54 +0100

    Separated time from main, added table innit

---
+++ table_files.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+def create_date_table(start='2000-01-01', end='2050-12-31'):
+    start_ts = pd.to_datetime(start).date()
+
+    end_ts = pd.to_datetime(end).date()
+
+    # record timetsamp is empty for now
+    dates = pd.DataFrame(columns=['Entry Hour', 'Leave Hour'], index=pd.date_range(start_ts, end_ts))
+    dates.index.name = 'Date'
+
+    days_names = {
+        i: name
+        for i, name
+        in enumerate(['Monday', 'Tuesday', 'Wednesday',
+                      'Thursday', 'Friday', 'Saturday',
+                      'Sunday'])
+    }
+
+    dates['Day'] = dates.index.dayofweek.map(days_names.get)
+    dates['Month'] = dates.index.month
+    dates.reset_index(inplace=True)
+    dates.index.name = 'date_id'
+    return dates
+
--- time_parts.py
+++ time_parts.py
@@ -1,16 +1,21 @@
-import os
 from datetime import date, datetime
-import pandas as pd
-from table_files import *
-# grab today's date
-today = date.today()
-print(f"Today is {today}")
+
+
+def grab_hour():
+    entry_hour = input("Enter work entry hour in HH:MM format please: ")
+    start_time = datetime.strptime(f"{entry_hour}:00", "%H:%M:%S")
+    return start_time
+
+def grab_endhour():
+    pass
+
 
 # grab today's entry time
 # TODO - add GUI
-entry_hour = input("Enter work entry hour in HH:MM format please: ")
+entry = grab_hour()
+
+
 leave_hour = input("Enter the hour you left the work in HH:MM format please: ")
-start_time = datetime.strptime(f"{entry_hour}:00", "%H:%M:%S")
 end_time = datetime.strptime(f"{leave_hour}:00", "%H:%M:%S")
 
 # get difference
@@ -19,6 +24,3 @@ worked_sec = delta.total_seconds()
 worked_hours = (worked_sec - (worked_sec % 3600)) / 3600
 worked_hours_float = worked_sec / 3660
 print(f"Total time worked is {worked_hours}:{(worked_sec % 3600)/60}")
-
-calendar = create_date_table(start='2024-01-01', end='2024-12-31')
-print(calendar)
