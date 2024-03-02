a8c7dc28a14c1e5a428fab323943c234e40d1ba6
Ian <jan.kosminski@gmail.com>
2024-03-02 10:54:00 +0100

    Update 1: Grab time Calculate time difference

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
-import time
 from datetime import date, datetime
+
+
 # grab today's date
 today = date.today()
 print(f"Today is {today}")
 
 # grab today's entry time
+# TODO - add GUI
 entry_hour = input("Enter work entry hour in HH:MM format please: ")
 leave_hour = input("Enter the hour you left the work in HH:MM format please: ")
 start_time = datetime.strptime(f"{entry_hour}:00", "%H:%M:%S")
@@ -13,15 +15,7 @@ end_time = datetime.strptime(f"{leave_hour}:00", "%H:%M:%S")
 
 # get difference
 delta = end_time - start_time
-
-sec = delta.total_seconds()
-print('difference in seconds:', sec)
-
-min = sec / 60
-print('difference in minutes:', min)
-
-# get difference in hours
-hours = (min - (min % 60)) / 60
-print('difference in hours:', hours)
-
-print(f"Total time worked is {hours}:{min % 60}")
+worked_sec = delta.total_seconds()
+worked_hours = (worked_sec - (worked_sec % 3600)) / 3600
+worked_hours_float = worked_sec / 3660
+print(f"Total time worked is {worked_hours}:{(worked_sec % 3600)/60}")
