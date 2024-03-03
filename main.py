7d421ff5994be24a6ff8bceb001e9af4ade321b6
Ian <jan.kosminski@gmail.com>
2024-03-03 20:21:37 +0100

    Added summing functionality - doesn't work if xlsx is tamepered and column is not string formatted

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import os
-from datetime import date, datetime
+from datetime import date
 import pandas as pd
-from table_operations import *
+import table_operations as top
 import openpyxl
 
 
-# initiate empty calendar if it doesn't exist else read xlsx and concat to single DF
+# initiate empty calendar if it doesn't exist, else read .xlsx and concat to single DF
 if not(os.path.isfile("calendar.xlsx")):
-    calendar = create_date_table(start='2024-01-01', end='2024-12-31')
-    create_excel_file(calendar)
+    calendar = top.create_date_table(start='2024-01-01', end='2024-12-31')
+    top.create_excel_file(calendar)
 else:
     calendar = pd.concat(pd.read_excel("calendar.xlsx", engine="openpyxl", sheet_name=None), ignore_index=True)
 
-print(calendar)
 # grab today's date and convert to ns64
 today = date.today()
+print(today)
 today_64 = pd.Timestamp(today).to_datetime64()
-print(f"Today is {today}")
-print(calendar['Date'].dtype)
-print(type(today))
+top.sum_hours(today, calendar)
 # grab hour to add
-got_hour = grab_hour()
-write_hour(today_64, calendar, str(got_hour))
-create_excel_file(calendar)
-
+# got_hour = top.grab_hour()
+# top.write_hour(today_64, calendar, str(got_hour))
+# top.create_excel_file(calendar)
--- table_operations.py
+++ table_operations.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import calendar
 from datetime import datetime
+from math import isnan
 import numpy as np
 
-months_list = [calendar.month_name[i] for i in range(1, 13)]
+MONTHS = [calendar.month_name[i] for i in range(1, 13)]
 
 
 def create_date_table(start='2000-01-01', end='2050-12-31'):
@@ -11,7 +12,7 @@ def create_date_table(start='2000-01-01', end='2050-12-31'):
     end_ts = pd.to_datetime(end).date()
     # record timestamp is empty for now
     dates = pd.DataFrame(columns=['Entry Hour', 'Leave Hour', 'Time Spent Working'],
-                         index=pd.date_range(start_ts, end_ts))
+                         index=pd.date_range(start_ts, end_ts), dtype='datetime')
     dates.index.name = 'Date'
     days_names = {
         i: name
@@ -39,7 +40,7 @@ def write_hour(date, dataframe, hour):
 
 def create_excel_file(dataframe):
     with pd.ExcelWriter('calendar.xlsx') as writer:
-        for i, j in enumerate(months_list, start=1):
+        for i, j in enumerate(MONTHS, start=1):
             page = dataframe.loc[dataframe['Month'] == i]
             page.to_excel(writer, sheet_name=j)
 
@@ -48,3 +49,17 @@ def grab_hour():
     hour = input("Enter hour in HH:MM format please: ")
     stripped_hour = datetime.strptime(f"{hour}:00", "%H:%M:%S")
     return stripped_hour
+
+def sum_hours(day, dataframe):
+    current_month = int(str(day)[5:7])
+    print(current_month)
+    total_hours = dataframe.loc[dataframe['Month'] == current_month, 'Time Spent Working'].dropna().values.tolist()
+    sum_h = 0
+    sum_m = 0
+    print(total_hours)
+    for i in total_hours:
+        sum_h += int(i[:2])
+        sum_m += int(i[3:])
+    sum_h += int((sum_m - (sum_m % 60)) / 60)
+    sum_m = sum_m % 60
+    print(sum_h,":", sum_m)
