2623465c278de1e1b38beacb045be1381b896d35
Jan <jan.kosminski@gmail.com>
2024-03-17 11:05:44 +0100

    Changed deprecated _set_value to new df.at[x,y] = val

--- table_operations.py
+++ table_operations.py
@@ -27,15 +27,14 @@ def create_date_table(start='2000-01-01', end='2050-12-31'):
 def write_hour(date, dataframe, hour):
     index = dataframe.isin([date]).any(axis=1).idxmax()
     if pd.isna(dataframe.loc[index, 'Entry Hour']):
-        dataframe._set_value(index, 'Entry Hour', hour)
+        dataframe.at[index, 'Entry Hour'] = hour
     else:
-        dataframe._set_value(index, 'Leave Hour', hour)
+        dataframe.at[index, 'Leave Hour'] = hour
         dataframe[['Entry Hour', 'Leave Hour']] = dataframe[['Entry Hour', 'Leave Hour']].apply(pd.to_datetime)
         dataframe['Time Spent Working'] = dataframe['Time Spent Working'].astype(object)
         diff_sec = pd.Timedelta(dataframe.iloc[index]['Leave Hour'] - dataframe.iloc[index]['Entry Hour']).seconds
         diff = f"{int((diff_sec - diff_sec % 3600)/ 3600.0)}:{int((diff_sec % 3600)/60)}"
-        dataframe._set_value(index, 'Time Spent Working', diff)
-
+        dataframe.at[index, 'Time Spent Working'] = diff
 
 
 def create_excel_file(dataframe):
